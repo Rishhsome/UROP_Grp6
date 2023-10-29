c4e049a1bb73b4e19a3a5544d45cbfe91034af79
Rishabh Chauhan <rishu.ch.789@gmail.com>
2023-10-29 16:06:04 +0530

    AES Encryption-Decryption completed

--- encryptionApproach.java
+++ encryptionApproach.java
@@ -7,6 +7,16 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
 public class encryptionApproach {
 
     private static final int NUM_EDGE_SERVERS = 8;
@@ -23,8 +33,13 @@ public class encryptionApproach {
             int downloadSpeed = (int) (Math.random() * 50) + 1;
             int uploadSpeed = (int) (Math.random() * 10) + 1;
             int dataSentSize = (int) (Math.random() * 1000) + 1;
+            long mn = 0;
+            for (int m = 0; m < 10; m++)
+                mn = mn * 10 + (long) (Math.random() * 10);
+            long mobileNumber = mn;
 
-            mobileDevices.add(new MobileDevice(dataProcessingSpeed, downloadSpeed, uploadSpeed, dataSentSize));
+            mobileDevices
+                    .add(new MobileDevice(dataProcessingSpeed, downloadSpeed, uploadSpeed, dataSentSize, mobileNumber));
         }
 
         // Create the edge servers
@@ -33,7 +48,13 @@ public class encryptionApproach {
             int downloadSpeed = (int) (Math.random() * 500) + 1;
             int uploadSpeed = (int) (Math.random() * 100) + 1;
 
-            edgeServers.add(new EdgeServer(dataProcessingSpeed, downloadSpeed, uploadSpeed));
+            // Get the mobile number of any random mobile device and use it as the key for a
+            // particular edge server
+            int randDevice = (int) (Math.random() * 20);
+            long mobileNumber = mobileDevices.get(randDevice).getMobileNumber();
+
+            edgeServers.add(new EdgeServer(dataProcessingSpeed, downloadSpeed, uploadSpeed,
+                    new AES_Encryption256(String.valueOf(mobileNumber))));
         }
 
         // Create the cloud servers
@@ -41,8 +62,13 @@ public class encryptionApproach {
             int dataProcessingSpeed = (int) (Math.random() * 10000) + 1;
             String hostname = "127.0.0.1";
             int port = 8080 + i;
-        
-            cloudServers.add(new CloudServer(dataProcessingSpeed, hostname, port));
+
+            // Similar process for
+            int randDevice = (int) (Math.random() * 20);
+            long mobileNumber = mobileDevices.get(randDevice).getMobileNumber();
+
+            cloudServers.add(new CloudServer(dataProcessingSpeed, hostname, port,
+                    new AES_Decryption256(String.valueOf(mobileNumber))));
         }
 
         // Create a thread pool to handle the mobile devices
@@ -57,9 +83,22 @@ public class encryptionApproach {
                 int uploadSpeed = mobileDevice.getUploadSpeed();
                 int dataSentSize = mobileDevice.getDataSentSize();
 
-                CloudServer cloudServer = findMostEfficientCloudServer(dataProcessingSpeed, downloadSpeed, uploadSpeed, dataSentSize);
-                System.out.println("Sending data from mobile device to edge server...");
-                edgeServer.sendDataToCloudServer(cloudServer, mobileDevice);
+                CloudServer cloudServer = findMostEfficientCloudServer(dataProcessingSpeed, downloadSpeed, uploadSpeed,
+                        dataSentSize);
+                System.out.println(
+                        "Sending data for Mobile Device " + mobileDevice.getMobileNumber() + " to Edge Server...");
+
+                // Generate a random string for this mobile device
+                String randomString = generateRandomString();
+                System.out.println(
+                        "Random String for Mobile Device " + mobileDevice.getMobileNumber() + ": " + randomString);
+
+                edgeServer.sendDataToCloudServer(cloudServer, mobileDevice, randomString);
+
+                // Process data received from the cloud server
+                System.out.println("Processing data for Mobile Device " + mobileDevice.getMobileNumber()
+                        + " received from the cloud server...");
+                cloudServer.processDataFromCloudServer(cloudServer, mobileDevice);
             });
         }
 
@@ -81,12 +120,15 @@ public class encryptionApproach {
         private int downloadSpeed;
         private int uploadSpeed;
         private int dataSentSize;
+        private long mobileNumber;
 
-        public MobileDevice(int dataProcessingSpeed, int downloadSpeed, int uploadSpeed, int dataSentSize) {
+        public MobileDevice(int dataProcessingSpeed, int downloadSpeed, int uploadSpeed, int dataSentSize,
+                long mobileNumber) {
             this.dataProcessingSpeed = dataProcessingSpeed;
             this.downloadSpeed = downloadSpeed;
             this.uploadSpeed = uploadSpeed;
             this.dataSentSize = dataSentSize;
+            this.mobileNumber = mobileNumber;
         }
 
         public int getDataProcessingSpeed() {
@@ -104,6 +146,10 @@ public class encryptionApproach {
         public int getDataSentSize() {
             return dataSentSize;
         }
+
+        public long getMobileNumber() {
+            return mobileNumber;
+        }
     }
 
     public static class EdgeServer {
@@ -112,10 +158,13 @@ public class encryptionApproach {
         private int downloadSpeed;
         private int uploadSpeed;
 
-        public EdgeServer(int dataProcessingSpeed, int downloadSpeed, int uploadSpeed) {
+        private AES_Encryption256 encryption;
+
+        public EdgeServer(int dataProcessingSpeed, int downloadSpeed, int uploadSpeed, AES_Encryption256 encryption) {
             this.dataProcessingSpeed = dataProcessingSpeed;
             this.downloadSpeed = downloadSpeed;
             this.uploadSpeed = uploadSpeed;
+            this.encryption = encryption;
         }
 
         public int getDataProcessingSpeed() {
@@ -130,23 +179,31 @@ public class encryptionApproach {
             return uploadSpeed;
         }
 
-        public void sendDataToCloudServer(CloudServer cloudServer, MobileDevice mobileDevice) {
+        public void sendDataToCloudServer(CloudServer cloudServer, MobileDevice mobileDevice, String randomString) {
             try (Socket socket = new Socket(cloudServer.getHostname(), cloudServer.getPort())) {
                 // Get the input and output streams
                 try (InputStream inputStream = socket.getInputStream();
-                     OutputStream outputStream = socket.getOutputStream()) {
-                    // Read the data from the mobile device
-                    byte[] data = new byte[1024];
-                    int bytesRead;
-                    while ((bytesRead = inputStream.read(data)) != -1) {
-                        // Write the data to the cloud server
-                        outputStream.write(data, 0, bytesRead);
-                    }
-                    System.out.println("Data sent successfully.");
+                        OutputStream outputStream = socket.getOutputStream()) {
+                    // Encrypt the random string
+                    String encryptedData = encryptData(randomString);
+
+                    // Print the encrypted string
+                    System.out.println("Encrypted String for Mobile Device " + mobileDevice.getMobileNumber() + ": "
+                            + encryptedData);
+
+                    // Write the encrypted data to the cloud server
+                    outputStream.write(encryptedData.getBytes(StandardCharsets.UTF_8));
+                    System.out.println(
+                            "Data sent successfully for Mobile Device " + mobileDevice.getMobileNumber() + ".");
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+
+        // Add a method to encrypt data
+        private String encryptData(String data) {
+            return encryption.encrypt(data);
         }
     }
 
@@ -156,10 +213,13 @@ public class encryptionApproach {
         private String hostname;
         private int port;
 
-        public CloudServer(int dataProcessingSpeed, String hostname, int port) {
+        private AES_Decryption256 decryption;
+
+        public CloudServer(int dataProcessingSpeed, String hostname, int port, AES_Decryption256 decryption) {
             this.dataProcessingSpeed = dataProcessingSpeed;
             this.hostname = hostname;
             this.port = port;
+            this.decryption = decryption;
         }
 
         public int getDataProcessingSpeed() {
@@ -173,5 +233,101 @@ public class encryptionApproach {
         public int getPort() {
             return port;
         }
+
+        public void processDataFromCloudServer(CloudServer cloudServer, MobileDevice mobileDevice) {
+            try (Socket socket = new Socket(cloudServer.getHostname(), cloudServer.getPort())) {
+                // Read the data from the edge server
+                byte[] data = new byte[1024];
+                int bytesRead;
+                try (InputStream inputStream = socket.getInputStream();
+                        OutputStream outputStream = socket.getOutputStream()) {
+                    while ((bytesRead = inputStream.read(data)) != -1) {
+                        // Decrypt the data using the provided decryption object
+                        String dataToDecrypt = new String(data, 0, bytesRead, StandardCharsets.UTF_8);
+                        String decryptedData = decryptData(dataToDecrypt);
+
+                        // Process the decrypted data as needed
+                        // For example, you can write it to another stream or perform other operations.
+                        outputStream.write(decryptedData.getBytes(StandardCharsets.UTF_8));
+                    }
+                    System.out.println(
+                            "Data processed successfully for mobile device : " + +mobileDevice.getMobileNumber());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Add a method to decrypt data
+        private String decryptData(String data) {
+            return decryption.decrypt(data);
+        }
+    }
+
+    static class AES_Encryption256 {
+        private String SECRET_KEY;
+        private static final String SALTVALUE = "abcdefg";
+
+        AES_Encryption256(String SecretKey) {
+            this.SECRET_KEY = SecretKey;
+        }
+
+        public String encrypt(String strToEncrypt) {
+            try {
+                byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+                IvParameterSpec ivspec = new IvParameterSpec(iv);
+                SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+                KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALTVALUE.getBytes(), 65536, 256);
+                SecretKey tmp = factory.generateSecret(spec);
+                SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
+                byte[] encryptedData = cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8));
+                return Base64.getEncoder().encodeToString(encryptedData);
+            } catch (Exception e) {
+                System.out.println("Error occurred during encryption: " + e.toString());
+            }
+            return null;
+        }
+    }
+
+    static class AES_Decryption256 {
+        private String SECRET_KEY;
+        private static final String SALTVALUE = "abcdefg";
+
+        AES_Decryption256(String SecretKey) {
+            this.SECRET_KEY = SecretKey;
+        }
+
+        public String decrypt(String strToDecrypt) {
+            try {
+                byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+                IvParameterSpec ivspec = new IvParameterSpec(iv);
+                SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+                KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALTVALUE.getBytes(), 65536, 256);
+                SecretKey tmp = factory.generateSecret(spec);
+                SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
+                byte[] decryptedData = cipher.doFinal(Base64.getDecoder().decode(strToDecrypt));
+                return new String(decryptedData, StandardCharsets.UTF_8);
+            } catch (Exception e) {
+                System.out.println("Error occurred during decryption: " + e.toString());
+            }
+            return null;
+        }
+    }
+
+    private static String generateRandomString() {
+        StringBuilder randomString = new StringBuilder();
+        int length = 10; // Change the length as needed
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (int i = 0; i < length; i++) {
+            int index = (int) (Math.random() * characters.length());
+            randomString.append(characters.charAt(index));
+        }
+
+        return randomString.toString();
     }
 }
